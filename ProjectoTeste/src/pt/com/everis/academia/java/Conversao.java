d034db638bd5cc7947dce81edf36e7cc382ecaf8
PedroFilipeVenancio <pedro.vanom@gmail.com>
2019-06-25 11:37:05 +0100

    usando metodos dos wrappers

--- ProjectoTeste/src/pt/com/everis/academia/java/Conversao.java
+++ ProjectoTeste/src/pt/com/everis/academia/java/Conversao.java
@@ -16,11 +16,31 @@ public class Conversao {
 		Integer i2 = new Integer(10);
 		System.out.println(i + " e " +i2);
 		
-		Character c = new Character('1');
+		Character c = new Character('f');
 		System.out.println(c);
 		
 		Boolean b = new Boolean("true");
 		System.out.println(b);
+		
+		Byte by = new Byte((byte) 1 );
+		System.out.println(by);
+		
+		Long lo = new Long(43);
+		System.out.println(lo);
+		System.out.println(lo.longValue());
+		System.out.println(lo.doubleValue());
+		System.out.println(lo.intValue());
+		System.out.println(lo.floatValue());
+		
+		Double d2 = new Double(3.14555);
+		System.out.println(d2.floatValue());
+		
+		Float f2 = new Float(3.4);
+		System.out.println(f2.doubleValue());
+		
+		System.out.println(f2.compareTo((float) f2.doubleValue()));
+		System.out.println(f2.getClass());
+		System.out.println(f2.isNaN());
 
 	}
 
---
+++ ProjectoTeste/src/pt/com/everis/academia/java/Wrappers.java
@@ -0,0 +1,74 @@
+package pt.com.everis.academia.java;
+
+public class Wrappers {
+
+	public static void main(String[] args) {
+		System.out.println("----------------------------------");
+		System.out.println("Byte - 66");
+		Byte b = new Byte((byte) 66);
+		System.out.println(b.byteValue());
+		System.out.println(b.doubleValue());
+		System.out.println(b.floatValue());
+		System.out.println(b.intValue());
+		System.out.println(b.longValue());
+		System.out.println(b.shortValue());
+		System.out.println("----------------------------------" + "\n");
+		
+		System.out.println("----------------------------------");
+		System.out.println("Double - 3.44");
+		Double d = new Double(3.44);
+		System.out.println(d.byteValue());
+		System.out.println(d.doubleValue());
+		System.out.println(d.floatValue());
+		System.out.println(d.intValue());
+		System.out.println(d.longValue());
+		System.out.println(d.shortValue());
+		System.out.println("----------------------------------" + "\n");
+		
+		System.out.println("----------------------------------");
+		System.out.println("Float - 3.4");
+		Float f = new Float(3.4);
+		System.out.println(f.byteValue());
+		System.out.println(f.doubleValue());
+		System.out.println(f.floatValue());
+		System.out.println(f.intValue());
+		System.out.println(f.longValue());
+		System.out.println(f.shortValue());
+		System.out.println("----------------------------------" + "\n");
+		
+		System.out.println("----------------------------------");
+		System.out.println("Integer - 77");
+		Integer i = new Integer(77);
+		System.out.println(i.byteValue());
+		System.out.println(i.doubleValue());
+		System.out.println(i.floatValue());
+		System.out.println(i.intValue());
+		System.out.println(i.longValue());
+		System.out.println(i.shortValue());
+		System.out.println("----------------------------------" + "\n");
+		
+		System.out.println("----------------------------------");
+		System.out.println("Long - 33333333333333333L");
+		Long l = new Long(33333333333333333L);
+		System.out.println(l.byteValue());
+		System.out.println(l.doubleValue());
+		System.out.println(l.floatValue());
+		System.out.println(l.intValue());
+		System.out.println(l.longValue());
+		System.out.println(l.shortValue());
+		System.out.println("----------------------------------" + "\n");
+		
+		System.out.println("----------------------------------");
+		System.out.println("Short - (short)88888888");
+		Short s = new Short((short)88888888);
+		System.out.println(s.byteValue());
+		System.out.println(s.doubleValue());
+		System.out.println(s.floatValue());
+		System.out.println(s.intValue());
+		System.out.println(s.longValue());
+		System.out.println(s.shortValue());
+		System.out.println("----------------------------------" + "\n");
+
+	}
+
+}
