cb98d43bf8360d75d7ee19ebdf10519e48be012d
PedroFilipeVenancio <pedro.vanom@gmail.com>
2019-06-25 10:28:22 +0100

    pequenas operações de conversão com alguns wrappers class´s

--- ProjectoTeste/src/pt/com/everis/academia/java/Conversao.java
+++ ProjectoTeste/src/pt/com/everis/academia/java/Conversao.java
@@ -2,8 +2,25 @@ package pt.com.everis.academia.java;
 
 public class Conversao {
 
+	static long teste = 9999999999999999L;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		double valor = 3.1415;
+		int mau = (int) teste;
+		System.out.println(mau);
+		
+		float teste2 = 4.4f;
+		double d = teste2;
+		
+		Integer i = new Integer("5");
+		Integer i2 = new Integer(10);
+		System.out.println(i + " e " +i2);
+		
+		Character c = new Character('1');
+		System.out.println(c);
+		
+		Boolean b = new Boolean("true");
+		System.out.println(b);
 
 	}
 
