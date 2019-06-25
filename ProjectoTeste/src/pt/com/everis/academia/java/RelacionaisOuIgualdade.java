4d48f3ba5b92c58fdb3b2128aa43bdb0142de35e
PedroFilipeVenancio <pedro.vanom@gmail.com>
2019-06-25 17:05:39 +0100

    RelacionaisOuIgualdade class

---
+++ ProjectoTeste/src/pt/com/everis/academia/java/RelacionaisOuIgualdade.java
@@ -0,0 +1,32 @@
+package pt.com.everis.academia.java;
+
+public class RelacionaisOuIgualdade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value1 = 1;
+		int value2 = 2;
+		
+		if (value1 == value2) {
+			System.out.println(false);
+		}
+		
+		if (value1 != value2) {
+			System.out.println(true);
+		}
+		
+		if (value1 > value2) {
+			System.out.println(false);
+		}
+		
+		if (value1 < value2) {
+			System.out.println(true);
+		}
+		
+		if (value1 <= value2) {
+			System.out.println(true);
+		}
+		
+	}
+
+}
