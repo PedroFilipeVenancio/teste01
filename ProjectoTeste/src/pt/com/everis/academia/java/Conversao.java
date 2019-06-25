f91b539f94ea8e91ad8eeb7d2ba05394d53d2a33
PedroFilipeVenancio <pedro.vanom@gmail.com>
2019-06-25 09:53:27 +0100

    adicionei conversao

---
+++ ProjectoTeste/src/pt/com/everis/academia/java/Conversao.java
@@ -0,0 +1,10 @@
+package pt.com.everis.academia.java;
+
+public class Conversao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ProjectoTeste/src/pt/com/everis/academia/java/PrimeiraClasse.java
+++ ProjectoTeste/src/pt/com/everis/academia/java/PrimeiraClasse.java
@@ -1,10 +1,13 @@
 package pt.com.everis.academia.java;
 
 public class PrimeiraClasse {
+	
+	static int lol = 999;
 
 	public static void main(String[] args) {
 		System.out.println("Primeiro projecto tester");
 		System.out.println("Joe shooter");
+		System.out.println(lol);
 
 	}
 
